634503fc493a0ba3c1b884df5afe6efccbccdf8f
RPi <RPi>
2018-03-23 22:36:30 +0000

    Molly - Now the joystick values are from -1 to 1, and left and right are printed

--- testCode/JoystickTelecomm/joystick_to_telecom.cpp
+++ testCode/JoystickTelecomm/joystick_to_telecom.cpp
@@ -9,6 +9,8 @@ int main(){
 		printf("open failed.\n");
 		exit(1);
 	  }
+  double left=0.0;
+  double right=0.0;
 	while(1){
 		usleep(1000);
 		JoystickEvent event;
@@ -16,16 +18,26 @@ int main(){
 		{
 			if (event.isButton())
 			{
-				printf("Button %u is %s\n", event.number, event.value == 0 ? "up" : "down");
+				//printf("Button %u is %s\n", event.number, event.value == 0 ? "up" : "down");
 				//if (event.number == 2)
 				//send
 			}
-			else if (event.isAxis())
+			else if (event.isAxis() && (event.number == 1 || event.number ==4))
 			{
-				printf("Axis %u is at position %d\n", event.number, event.value);
+				//printf("Axis %u is at position %d\n", event.number, event.value);
+        if (event.number == 1){
+          left=event.value;
+        }
+        if (event.number == 4){
+          right=event.value;
+        }
+        //printf("%f    %f\n",left,right);
+        double sleft= (int)(left/0.001/32767.0)*-0.001;
+        double sright= (int)(right/0.001/32767.0)*-0.001;
+        printf("%f    %f\n",sleft,sright);
 				//send y axis scale from -1 to 1 for left and right(example: "0.2132145 -0.2436823")
 			}
 		}
 	}
 	return 0;
-} //left vertical = 1 right vertical = 4
+}
