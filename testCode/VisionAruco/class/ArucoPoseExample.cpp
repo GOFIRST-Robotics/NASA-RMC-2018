3752637b1a6a8d904f1270a026a5774ed9ba1e91
Matt <tlach007@umn.edu>
2018-04-14 16:06:01 -0500

    Adding Aruco class files, currently untested because it won't compile

---
+++ testCode/VisionAruco/class/ArucoPoseExample.cpp
@@ -0,0 +1,44 @@
+#include "ArucoPose.h"
+#include "ArucoPose.cpp"
+
+#include "aruco/aruco.h"
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/videoio.hpp"
+#include <string>
+#include <stdexcept>
+
+using namespace cv;
+using namespace std;
+using namespace aruco;
+
+int main(int argc, char** argv){
+    try{
+		cv::Mat InImage;
+        // Open input and read image
+        VideoCapture vreader("webcamImage.jpg");
+        if (vreader.isOpened()) vreader >> InImage;
+        else throw std::runtime_error("Could not open input");
+
+		ArucoPose pose;
+		cout << "Initialized. Ready to track: " << pose.isReady() << endl;
+		pose.setCameraParameters("webcamcalibration.yml");
+		cout << "Parameters set. Ready to track: " << pose.isReady() << endl;
+		pose.setMarkerMap("aruco_calibration_grid_board_a4.yml");
+		cout << "Marker map defined. Ready to track: " << pose.isReady() << endl;
+		pose.setMarkerSize(0.03614);
+		cout << "Marker map defined. Ready to track: " << pose.isReady() << endl;
+
+		pose.updatePose(InImage);
+		cout << "Transform matrix: " << endl;
+		cout << pose.getRTmatrix() << endl;
+		cout << "Translation vector: " << endl;
+		cout << pose.getTvec() << endl;
+		cout << "Rotation vector: " << endl;
+		cout << pose.getRvec() << endl;
+	} catch (std::exception& ex){
+        cout << "Exception :" << ex.what() << endl;
+    }
+}
