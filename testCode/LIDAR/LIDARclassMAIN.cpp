7530e4c327a3774e738f7b8f7a7bb100fad9f7d0
MOLLYBAS <schw1818@umn.edu>
2018-04-14 10:52:57 -0500

    idk how to not have these overwritten but also not commit them so I'm commiting them, but they are junk so just ignore

--- testCode/LIDAR/LIDARclassMAIN.cpp
+++ testCode/LIDAR/LIDARclassMAIN.cpp
@@ -6,25 +6,22 @@ double PI = 3.14159265;
 double toRadians(double angle, double PI);
 using namespace std;
 
-int main()
-{
+int main(){
 	//angles must be in radians
-	double angle=1.0;
+	double angle=30
 	double distance=30.0;
 	double height=20.0;
 	
 	double minangle=-1000000.0;
 	double maxangle=1000000.0;
-	if (angle > minangle && angle < maxangle)
-	{
+	if (angle > minangle && angle < maxangle){
 		Lidar test=Lidar(toRadians(angle,PI), distance, height); //math requires radians
 		test.printcoords(cout);
 	}
 	return 0;
 }
 
-double toRadians(double angle, double PI)
-{
+double toRadians(double angle, double PI){
 	double rad=(angle*PI/180.0);
 	return rad;
 }
