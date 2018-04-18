bba13199872698e63e1d4dae82133f874a7ebde1
Andidy <andrewyantos@gmail.com>
2018-04-17 20:53:09 -0500

    Updated some of NavX.cpp to work with the SensorFusion algorithm, however there are theory questions that need to be answered before it can be completed

--- testCode/DeadReckoning/IMU.h
+++
@@ -1,8 +0,0 @@
-// Define either MADGWICK or MAHONY as IMU filters
-#define MADGWICK
-
-#ifdef MADGWICK
-  #include "Madgwick.h"
-#elif MAHONY
-  #include "Mahony.h"
-#endif
--- testCode/DeadReckoning/NavX.cpp
+++ testCode/DeadReckoning/NavX.cpp
@@ -1,4 +1,11 @@
 #include "NavX.h"
+#include <cmath>
+
+#ifdef MADGWICK
+  #include "Madgwick.h"
+#elif MAHONY
+  #include "Mahony.h"
+#endif
 
 std::shared_ptr<AHRS> ahrs;
 
@@ -16,11 +23,9 @@ bool NavX::IsRotating(){ return ahrs->IsRotating(); }
 bool NavX::IsConnected(){ return ahrs->IsConnected(); }
 void NavX::Close(){ ahrs->Close(); }
 
-double NavX::GetByteCount(){ return ahrs->GetByteCount(); }
-double NavX::GetUpdateCount(){ return ahrs->GetUpdateCount(); }
-
 long NavX::GetLastSensorTimestamp(){ return ahrs->GetLastSensorTimestamp(); }
 
+//GetVelocity and GetDisplacement functions need to be updated
 float NavX::GetVelocityX(){ return ahrs->GetVelocityX(); }
 float NavX::GetVelocityY(){ return ahrs->GetVelocityY(); }
 float NavX::GetVelocityZ(){ return ahrs->GetVelocityZ(); }
@@ -28,28 +33,51 @@ float NavX::GetVelocityZ(){ return ahrs->GetVelocityZ(); }
 float NavX::GetDisplacementX(){ return ahrs->GetDisplacementX(); }
 float NavX::GetDisplacementY(){ return ahrs->GetDisplacementY(); }
 float NavX::GetDisplacementZ(){ return ahrs->GetDisplacementZ(); }
+
+// Yaw aka Header formula yaw = tan^-1( 2(q1 * q2 + q3 * q0), q1^2 - q2^2 - q3^2 + q4^2)
+float  NavX::GetYaw(){ 
+  return atan2( 2(q1 * q2 + q3 * q0), q1^2 - q2^2 - q3^2 + q4^2)
+}
+// Pitch formula pitch = sin^-1( 2(q1 * q3 - q2 * q0) / (q1^2 + q2^2 + q3^2 + q0^2) )
+float  NavX::GetPitch(){ 
+  return asin( 2(q1 * q3 - q2 * q0) / (q1^2 + q2^2 + q3^2 + q0^2) )
+}
+// Roll formula roll = tan^-1( 2(q2 * q3 + q1 * q0), -q1^2 - q2^2 + q3^2 + q4^2)
+float  NavX::GetRoll(){ 
+  return atan2( 2(q2 * q3 + q1 * q0), -q1^2 - q2^2 + q3^2 + q4^2)
+}
+
+
+float NavX::GetWorldLinearAccelX(){ return 9.81*(ahrs->GetWorldLinearAccelX()); }
+float NavX::GetWorldLinearAccelY(){ return 9.81*(ahrs->GetWorldLinearAccelY()); }
+float NavX::GetWorldLinearAccelZ(){ return 9.81*(ahrs->GetWorldLinearAccelZ()); }
+
+//GetQuaternions should be fine
+float  NavX::GetQuaternionW(){ return q0; }
+float  NavX::GetQuaternionX(){ return q1; }
+float  NavX::GetQuaternionY(){ return q2; }
+float  NavX::GetQuaternionZ(){ return q3; }
+
+#ifdef MADGWICK
+  //NEED TO CORRECT VARIABLE INPUTS NOT CORRECT (ahrs->Get[type of rotation] vs ahrs->GetRawGyro(X,Y,Z))
+  MadgwickAHRSupdateIMU(ahrs->GetPitch(), ahrs->GetRoll(), ahrs->GetYaw(), float ax, float ay, float az);
+#elif MAHONY
+  MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az)
+#endif
+
+//Below is stuff I don't know if we need
+double NavX::GetAngle(){ return ahrs->GetAngle(); }
+double NavX::GetRate(){ return ahrs->GetRate(); }
+void   NavX::Reset(){ ahrs->Reset(); }
+
 void  NavX::ResetDisplacement(){ ahrs->ResetDisplacement(); }
 void  NavX::UpdateDisplacement( float accel_x_g, float accel_y_g,
                           int update_rate_hz, bool is_moving ){
   ahrs->UpdateDisplacement(accel_x_g, accel_y_g, update_rate_hz, is_moving);
 }
 
-float NavX::GetWorldLinearAccelX(){ return ahrs->GetWorldLinearAccelX(); }
-float NavX::GetWorldLinearAccelY(){ return ahrs->GetWorldLinearAccelY(); }
-float NavX::GetWorldLinearAccelZ(){ return ahrs->GetWorldLinearAccelZ(); }
-
-float  NavX::GetYaw(){ return ahrs->GetYaw(); }
-double NavX::GetAngle(){ return ahrs->GetAngle(); }
-double NavX::GetRate(){ return ahrs->GetRate(); }
-void   NavX::Reset(){ ahrs->Reset(); }
-
-float  NavX::GetPitch(){ return ahrs->GetPitch(); }
-float  NavX::GetRoll(){ return ahrs->GetRoll(); }
-
-float  NavX::GetQuaternionW(){ return ahrs->GetQuaternionW(); }
-float  NavX::GetQuaternionX(){ return ahrs->GetQuaternionX(); }
-float  NavX::GetQuaternionY(){ return ahrs->GetQuaternionY(); }
-float  NavX::GetQuaternionZ(){ return ahrs->GetQuaternionZ(); }
+double NavX::GetByteCount(){ return ahrs->GetByteCount(); }
+double NavX::GetUpdateCount(){ return ahrs->GetUpdateCount(); }
 
 /*---Native Functions That Are Not Implemented---*/
 float NavX::GetRawAccelX(){ return ahrs->GetRawAccelX(); }
