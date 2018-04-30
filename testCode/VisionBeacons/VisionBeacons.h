134989daca8bc965a1379c74bda6fca8d9f3cab0
Miny <jeong242@umn.edu>
2018-04-29 22:29:00 -0500

    Minor change

--- testCode/VisionBeacons/VisionBeacons.h
+++ testCode/VisionBeacons/VisionBeacons.h
@@ -2,7 +2,6 @@
 #define VISIONBEACONS_H
 
 #include <opencv2/objdetect/objdetect.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d.hpp>
