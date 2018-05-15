8942ad3c62ae46ad19c8585e7c1a959e96c6535f
RPi <RPi>
2018-05-15 18:13:16 +0000

    Jude: working on RPI, caught in half between versions. Need to separate.

--- testCode/Main_Exec/RPI_Main.cpp
+++ testCode/Main_Exec/RPI_Main.cpp
@@ -10,6 +10,7 @@
 
 #include <serial/serial.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 #include "Telecomm.h"
@@ -37,25 +38,31 @@ struct Timer {
   }
 };
 
+/*// Opened cap, camera No, send on comm line
+void sendCamera(cv::VideoCapture cap, int id, Telecomm& cm){
+  cv::Mat frame, image, gray, out;
+  do{
+    cap >> frame;
+  }while(frame.empty());
+  frame.copyTo(image);
+  cv::cvtColor(image, gray, cv::COLOR_BGR2GRAY);
+  cv::resize(gray, out, cv::Size(160,120), 0, 0, cv::INTER_AREA);
+  out = (out.reshape(0,1));
+  out.at<uchar>(0,0) = id;
+  cm.sendBytes((char*)out.data,out.total()*out.elemSize());
+}
+*/
 int main(){
   // Initialize classes
   
   // Telecomm
-  //Telecomm comm("192.168.1.50",5001,5001);
-  Telecomm comm("128.217.226.92",5001,5001);
+  Telecomm comm("192.168.1.50",5001,5001);
   comm.setFailureAction(false);
   comm.setBlockingTime(0,0);
   if(comm.status() != 0){
     printf("Error: %s\n", comm.verboseStatus().c_str());
     return comm.status();
   }
-/*  Telecomm commBytes("192.168.1.50",5002,5002);
-  commBytes.setFailureAction(false);
-  commBytes.setBlockingTime(0,0);
-  if(commBytes.status() != 0){
-    printf("Error: %s\n", commBytes.verboseStatus().c_str());
-    return commBytes.status();
-  }*/
 
   // Formatter
   val_fmt motor_msg_fmt = {
@@ -110,7 +117,7 @@ int main(){
   std::vector<int> motorState(5,1500);
   std::vector<int> motorStateLast(5,1500);
   bool imgshowState[8] = {false}; // See below
-  bool* teleopControl = &imgshowState[7]; // Piggyback on this fmt
+ /* bool* teleopControl = &imgshowState[7]; // Piggyback on this fmt
   std::vector<cv::Mat> imgshowFrames;
   for(int i = 0; i < 7; ++i){
     cv::Mat frame = cv::Mat::zeros(160,120,CV_8U);
@@ -118,7 +125,7 @@ int main(){
     imgshowFrames.push_back(frame);
   }
   int imgSize = imgshowFrames[0].total() * imgshowFrames[0].elemSize();
-
+*/
   // Timers
   Timer ardHeartbeatRecv = Timer(2000);
   Timer ardHeartbeatSend = Timer(500);
@@ -127,14 +134,21 @@ int main(){
   Timer cameraRequests[6] = {Timer(500),Timer(500),Timer(500),
                              Timer(500),Timer(500),Timer(500)};
   Timer tstTimer = Timer(500);
-  Clock::time_point t_ardBeat = Clock::now();
 
+ /* // Video Capture: cameras Back 0, Rfront 2, Rback 4
+  cv::VideoCapture cap0, cap2, cap4;
+  cap0.open("/dev/video0");
+  cap2.open("/dev/video1");
+  cap4.open("/dev/video2");
+  auto sendFrame0 = std::async(std::launch::async, [&](){sendCamera(cap0,0,comm);});
+  auto sendFrame2 = std::async(std::launch::async, [&](){sendCamera(cap2,2,comm);});
+  auto sendFrame4 = std::async(std::launch::async, [&](){sendCamera(cap4,4,comm);});
+*/
   // Loop
   while(1){
     // Updates
     t = Clock::now();
     comm.update();
-    //commBytes.update();
 
     // MC In
     std::string MC_msg_in;
@@ -151,11 +165,11 @@ int main(){
           imgshowState[iv.i] = iv.v;
         }
       }
-    }else if(mcHeartbeat.isTriggered()){
+   /* }else if(mcHeartbeat.isTriggered()){
       std::cout << "Failure: No heartbeat from MC to Pi\n";
       for(int i = 0; i < 5; ++i){
         motorState[i] = 1500;
-      }
+      }*/
     }
     
     // Arduino In; Use futures to rm lag?
@@ -207,14 +221,26 @@ int main(){
 
     // MC Out (Stats & messages through comm, images through commBytes)
     // Images
-    for(int i = 0; i < 7; ++i){
-      if(imgshowState[i]){
-        *(imgshowFrames[i].ptr(0,0)) = i; // This is an ID tag, unnoticable
+    /*for(int i = 0; i < 7; ++i){
+    /if(imgshowState[i]){
+        // *(imgshowFrames[i].ptr(0,0)) = i; // This is an ID tag, unnoticable
         //commBytes.sendBytes(imgshowFrames[i].data,imgSize);
       }
+    }*/
+  /*  if(sendFrame0.valid() && sendFrame0.wait_for(Millis(1)) == std::future_status::ready){
+      sendFrame0.get();
+      auto sendFrame0 = std::async(std::launch::async, [&](){sendCamera(cap0,0,comm);});
+    }
+    if(sendFrame2.valid() && sendFrame2.wait_for(Millis(1)) == std::future_status::ready){
+      sendFrame2.get();
+      auto sendFrame2 = std::async(std::launch::async, [&](){sendCamera(cap2,2,comm);});
+    }
+    if(sendFrame4.valid() && sendFrame4.wait_for(Millis(1)) == std::future_status::ready){
+      sendFrame4.get();
+      auto sendFrame4 = std::async(std::launch::async, [&](){sendCamera(cap0,4,comm);});
     }
     // Messages?
-
+*/
     // Heartbeat
     //if(heartbeat.isTriggered()){ // This one is supposed to trigger, instigator
 //      comm.send(".\n");
@@ -232,6 +258,7 @@ int main(){
       printf("Rebooting Connection\n");
       comm.reboot();
     }
+
   }
   return 0;
 }
