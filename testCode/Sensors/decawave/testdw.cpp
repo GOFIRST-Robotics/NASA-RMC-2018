ba88bb50c5d28bb6e18905b935589f433b914a53
MOLLYBAS <schw1818@umn.edu>
2018-05-15 02:20:55 -0400

    Added parsing guide and now test compiles

--- testCode/Sensors/decawave/testdw.cpp
+++ testCode/Sensors/decawave/testdw.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 #include <iostream>
 #include <math.h>
-
+//g++ testdw.cpp -std=c++11 -lserial
 #include <serial/serial.h>
 int main(){
 
   //Test to see if it can find the port
-  std::string port = ""; // could be something else
+  std::string port = "/dev/serial0"; // could be something else
   // Find serial ports
   std::vector<serial::PortInfo> devices_found = serial::list_ports();
   std::vector<serial::PortInfo>::iterator iter = devices_found.begin();
@@ -22,13 +22,15 @@ int main(){
   //Send command
   serial::Serial my_serial(port, 115200);
   if(my_serial.isOpen()){
-    my_serial.write(std::vector<unsigned char){0x0c, 0x00}>;
+    my_serial.write((std::vector <unsigned char>){0x0c});//, 0x00});
   }
+  std::cout<<"has written to port"<<std::endl;
 
   //Get back data
   unsigned char result[100];
   std::memset(result, 0, sizeof result);
-  my_serial.read(result,0x51);
+  my_serial.read(result,0x51);//#size to read, what char to delimit it by
+  std::cout<<"received data:"<<std::endl;
 
   //See what the heck the data looks like
   for(int i = 0; i < 100; ++i){
