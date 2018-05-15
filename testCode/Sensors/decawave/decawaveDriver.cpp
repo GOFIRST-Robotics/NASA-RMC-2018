3175214fba24205591fbe843f89a158fd16c6760
MOLLYBAS <schw1818@umn.edu>
2018-05-15 13:15:45 -0400

    added class driver for testing

--- testCode/Sensors/decawave/decawaveDriver.cpp
+++ testCode/Sensors/decawave/decawaveDriver.cpp
@@ -8,5 +8,19 @@
 #include <serial/serial.h>
 
 int main(){
+  Decawave::Decawave piTag;
+  //if error, may need a delay?
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  piTag.updateSamples();
+  Decawave::coordinate tagPos= piTag.getPos();
+  std::cout<<std::endl<<" "<<tagPos.x<<" "<<tagPos.y<<" "<<std::endl;
   return 0;
 }
